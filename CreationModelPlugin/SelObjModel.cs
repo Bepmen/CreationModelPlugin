0460491b27a106ec40e56c36d156c293f0533757
Александр Мигилев <e-yh@outlook.com>
2022-04-15 13:04:55 +0300

    Add project files.

---
+++ CreationModelPlugin/SelObjModel.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationModelPlugin
+{
+    public class SelObjModel
+    {
+        public static void SelectLevels(Document doc, out Level level1, out Level level2)
+        {
+            List<Level> listLevel = new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .OfType<Level>()
+                .ToList();
+
+            level1 = listLevel
+                .Where(x => x.Name.Equals("Уровень 1"))
+                .FirstOrDefault();
+            level2 = listLevel
+                .Where(x => x.Name.Equals("Уровень 2"))
+                .FirstOrDefault();
+        }
+    }
+}
